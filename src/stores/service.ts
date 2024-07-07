669dcb1d5a31282c549fd040196a718d3e9daada
luoqiz <851092732@qq.com>
2024-07-08 07:57:41 +0800

    feat: 添加防抖功能

--- src/stores/service.ts
+++ src/stores/service.ts
@@ -1,43 +1,61 @@
-import { invoke } from '@tauri-apps/api/core';
-import { defineStore } from 'pinia'
-import { ref } from 'vue'
+import { invoke } from "@tauri-apps/api/core";
+import { defineStore } from "pinia";
+import { ref } from "vue";
 import { listen } from "@tauri-apps/api/event";
-import { BaseResponse } from '~/types/base';
-import { UserInfo } from '~/types/contact';
-import { useConfigStore } from './config';
+import { BaseResponse } from "~/types/base";
+import { UserInfo } from "~/types/contact";
+import { useConfigStore } from "./config";
+import { useDebounceFn } from "@vueuse/core";
 
-export const useServiceStore = defineStore('service', () => {
-    // 当前状态是否运行
-    const isRunning = ref<boolean>(false);
-    // 当前登录用户信息 
-    const userInfo = ref<UserInfo>();
+export const useServiceStore = defineStore("service", () => {
+  // 当前状态是否运行
+  const isRunning = ref<boolean>(false);
+  // 当前登录用户信息
+  const userInfo = ref<UserInfo>();
+  // 使用配置服务
+  const configStore = useConfigStore();
 
-    const configStore = useConfigStore();
-    // 启动服务
-    const startService = async () => {
-        await invoke('start_server', { "host": "0.0.0.0", "port": 10010, "cburl": configStore.kConfig.cburl, "wsurl": configStore.kConfig.wsurl });
-        isRunning.value = true;
-        let result: BaseResponse<UserInfo> =  await invoke("get_user_info");
-        userInfo.value = result.data
+  // 启动服务
+  const startService = useDebounceFn(
+    async () => {
+      await invoke("start_server", {
+        host: "0.0.0.0",
+        port: 10010,
+        cburl: configStore.kConfig.cburl,
+        wsurl: configStore.kConfig.wsurl,
+      });
+      isRunning.value = true;
+      let result: BaseResponse<UserInfo> = await invoke("get_user_info");
+      userInfo.value = result.data;
+    },
+    1000,
+    { maxWait: 5000 }
+  );
+
+  // 关闭服务
+  const stopService = async () => {
+    await invoke("stop_server");
+    isRunning.value = false;
+  };
+  // 确认退出的弹窗
+  async function confirmExit() {
+    const shouldExit = await confirm("退出将无法使用服务，确定要退出吗？");
+    if (shouldExit) {
+      await invoke("confirm_exit");
     }
-    // 关闭服务
-    const stopService = async () => {
-        await invoke('stop_server');
-        isRunning.value = false;
-    }
-    // 确认退出的弹窗
-    async function confirmExit() {
-        const shouldExit = await confirm("退出将无法使用服务，确定要退出吗？");
-        if (shouldExit) {
-            await invoke('confirm_exit');
-        }
-    }
-    // 开启退出服务的监听
-    async function startExitEventListener() {
-        await listen('request-exit', () => {
-            confirmExit();
-        });
-    }
-  
-    return { isRunning, userInfo, startService, stopService, startExitEventListener}
-  })
+  }
+  // 开启退出服务的监听
+  async function startExitEventListener() {
+    await listen("request-exit", () => {
+      confirmExit();
+    });
+  }
+
+  return {
+    isRunning,
+    userInfo,
+    startService,
+    stopService,
+    startExitEventListener,
+  };
+});
