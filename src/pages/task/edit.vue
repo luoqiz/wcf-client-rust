0aeb7b924e8229c98abb50378ad13ffaf25570a5
luoqiz <851092732@qq.com>
2024-09-07 17:09:21 +0800

    fix: 打包

--- src/pages/task/edit.vue
+++ src/pages/task/edit.vue
@@ -18,7 +18,7 @@ const contact_org = ref<Contact[]>([]);
 // 目标用户
 const contact_target = ref<Contact[]>([]);
 
-watch(contact_select,()=>{
+watch(contact_select, () => {
   contact_org.value = contact_select.value;
 });
 
@@ -30,30 +30,29 @@ const current_task = ref<ForwardTask>({
   to_wxid_list: []
 })
 
-const add_task = ()=>{
-  current_task.value.from_wxid_list = contact_org.value.map(item => {return item.wxid});
-  current_task.value.to_wxid_list = contact_target.value.map(item => {return item.wxid});
-  task_save(serviceStore.userInfo!.wxid, current_task.value );
+const add_task = () => {
+  current_task.value.from_wxid_list = contact_org.value.map(item => { return item.wxid });
+  current_task.value.to_wxid_list = contact_target.value.map(item => { return item.wxid });
+  task_save(serviceStore.userInfo!.wxid, current_task.value);
 }
 
 onMounted(async () => {
-  if(serviceStore.isRunning){
+  if (serviceStore.isRunning) {
     contacts.value = await get_contacts();
   }
 });
- 
+
 </script>
 
 <template>
   <div class="w-full flex flex-col gap-16px">
-    <a-input :style="{width:'320px'}" placeholder="此任务名称" v-model="current_task.remark"/>
-    <a-select class="w-full" placeholder="选择参与的用户" 
-        multiple v-model="contact_select" :filter-option="true">
-      <a-option v-for="item of contacts" :value="item">{{item.remark}}</a-option>
+    <a-input :style="{ width: '320px' }" placeholder="此任务名称" v-model="current_task.remark" />
+    <a-select class="w-full" placeholder="选择参与的用户" multiple v-model="contact_select" :filter-option="true">
+      <a-option v-for="item of contacts" :value="item">{{ item.remark }}</a-option>
     </a-select>
 
     <div class="card">
-        <a-transfer
+      <!-- <a-transfer
           show-search
           :data="contact_org"
           v-model="contact_target"
@@ -64,7 +63,7 @@ onMounted(async () => {
             placeholder:'target item search'
           }"
         >
-        </a-transfer>
+        </a-transfer> -->
     </div>
     <a-button type="primary" @click="add_task">保存</a-button>
   </div>
--- src/types/contact.d.ts
+++ src/types/contact.d.ts
@@ -1,20 +1,20 @@
 export type Contact = {
-    city: string;
-    code: string;
-    country: string;
-    gender: number;
-    name: string;
-    province: string;
-    remark: string;
-    wxid: string;
-    label: string;
-    value: string;
-}
+  city: string;
+  code: string;
+  country: string;
+  gender: number;
+  name: string;
+  province: string;
+  remark: string;
+  wxid: string;
+  label: string;
+  value: string;
+  disabled: boolean;
+};
 
 export type UserInfo = {
-    wxid: string;
-    name: string;
-    mobile: string;
-    home: string;
-}
-
+  wxid: string;
+  name: string;
+  mobile: string;
+  home: string;
+};
